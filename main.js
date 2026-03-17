d68a6dfaaaad0d19da6e9f5b56f8b97d110b3719
kbob <kjartanjohannesen@gmail.com>
2026-03-17 22:02:15 +0100

    add UI

--- main.js
+++ main.js
@@ -44,53 +44,79 @@ function runX(x) {
 function getHumanChoice() {
     let humanChoice = prompt("Rock, Paper, Scissors?");
     let loweredChoice = humanChoice.toLowerCase();
-    if (!(loweredChoice == 'rock' || loweredChoice == 'paper' || loweredChoice == 'scissors')) {
-        console.log("ERRROR!")
-        return null;
+    if (loweredChoice == 'rock' || loweredChoice == 'paper' || loweredChoice == 'scissors') {
+        return loweredChoice;
     }
-    return loweredChoice;
+    console.log("ERRROR!")
+    return null;
     }
 
 let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
+    const result = document.querySelector("#result");
+    const score = document.querySelector("#score");
+
     if (humanChoice === computerChoice) {
-        console.log("Undecided! You both selected: ", humanChoice);
+        /* console.log("Undecided! You both selected: ", humanChoice); */
+        result.innerHTML = `Undecided! You both selected: ${humanChoice}`;
     } else switch(humanChoice) {
         case 'rock':
             if (computerChoice === 'paper') {
-                console.log("You lose! Paper beats Rock");
+                /* console.log("You lose! Paper beats Rock"); */
+                result.innerHTML = "You lose! Paper beats Rock";
                 computerScore += 1;
             } else {
-                console.log("You win! Rock beats Scissors");
+                /* console.log("You win! Rock beats Scissors"); */
+                result.innerHTML = "You win! Rock beats Scissors";
                 humanScore += 1;
             }
             break
         case 'paper':
             if (computerChoice === 'scissors') {
-                console.log("You lose! Scissors beats Paper");
+                /* console.log("You lose! Scissors beats Paper"); */
+                result.innerHTML = "You lose! Scissors beats Paper";
                 computerScore += 1;
             } else {
-                console.log("You win! Paper beats Rock");
+                /* console.log("You win! Paper beats Rock"); */
+                result.innerHTML = "You win! Paper beats Rock";
                 humanScore += 1;
             }
             break
         case 'scissors':
             if (computerChoice === 'rock') {
-                console.log("You lose! Rock beats Scissors");
+                /* console.log("You lose! Rock beats Scissors"); */
+                result.innerHTML = "You lose! Rock beats Scissors";
                 computerScore += 1;
             } else {
-                console.log("You win! Scissors beats Paper");
+                /* console.log("You win! Scissors beats Paper"); */
+                result.innerHTML = "You win! Scissors beats Paper";
                 humanScore += 1;
             }
             break
         default:
-            console.log("ERROR!");
+            /* console.log("ERROR!"); */
+            result.innerHTML = "ERROR!";
+
+    }
+    score.innerHTML = `Human: ${humanScore}, Computer ${computerScore}`;
+    if (humanScore == 5) {
+        score.innerHTML = `Hooray! You won the game with ${humanScore} against ${computerScore}`;
+        humanScore = 0;
+        computerScore = 0;
+    }
+    else if (computerScore == 5) {
+        score.innerHTML = `Too bad :( You lost the game with ${humanScore} against ${computerScore}`;
+        humanScore = 0;
+        computerScore = 0;
+    }
+    else {
+        score.innerHTML = `Human: ${humanScore}, Computer ${computerScore}`;
     }
 }
 
-function playGame() {
+/* function playGame() {
     for (i = 0; i < 5; i++) {
         playRound(getHumanChoice(), getComputerChoice());
     }
@@ -103,4 +129,23 @@ function playGame() {
     else {
         console.log("You lose! With " + humanScore + " vs. " + computerScore + " points ");
     }
-}
+} */
+
+const btnRock = document.querySelector("#rock");
+const btnPaper = document.querySelector("#paper");
+const btnScissors = document.querySelector("#scissors");
+
+btnRock.addEventListener("click", event => {
+    event.preventDefault();
+    playRound('rock', getComputerChoice());
+})
+
+btnPaper.addEventListener("click", event => {
+    event.preventDefault();
+    playRound('paper', getComputerChoice());
+})
+
+btnScissors.addEventListener("click", event => {
+    event.preventDefault();
+    playRound('scissors', getComputerChoice());
+})
